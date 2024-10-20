6a7982f93c756b945752e1fa07e87ddea290b1e2
MARKETTV1 <aliridakarim8@gmail.com>
2024-10-21 01:17:51 +0200

    Update JKinstall.sh

--- JKinstall.sh
+++ JKinstall.sh
@@ -5,10 +5,8 @@ wget -O /tmp/enigma2-plugin-extensions-jk-multi-mod_2.1_all.ipk https://github.c
 
 
 https://github.com/MARKETTV1/plugins/raw/refs/heads/main/enigma2-plugin-extensions-jk-multi-mod_2.1_all.ipk
-
-
-
+echo "Please wait a moment";
+wait
 opkg install /tmp/*.ipk
-
 && init 4; sleep 4; init 3
 
